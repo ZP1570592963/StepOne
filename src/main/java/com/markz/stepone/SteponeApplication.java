78e4246839f29db8b63b64bc2399ea54d13b881e
DESKTOP-CKDKN1R\15705 <1570592963@qq.com>
2021-01-01 16:24:55 +0800

    初始化

---
+++ src/main/java/com/markz/stepone/SteponeApplication.java
@@ -0,0 +1,13 @@
+package com.markz.stepone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteponeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteponeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/markz/stepone/SteponeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markz.stepone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteponeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
